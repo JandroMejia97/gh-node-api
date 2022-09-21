738f2259f527f6a41414943a518314c80c8abc7d
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-09-21 17:58:24 -0300

    Adding port for deploy in heroku

--- src/config/index.js
+++ src/config/index.js
@@ -3,5 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  serverPort: process.env.SERVER_PORT || 3000,
+  serverPort: process.env.SERVER_PORT || process.env.PORT || 3000,
 }
